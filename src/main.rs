6a03a84af6b9b52e9deddf61b84da66ea58212d6
Ethan Armstrong <w.ethan.armstrong@gmail.com>
2022-08-14 00:14:27 +0100

    split examples into seperate test functions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use crate::radio::{Radio, RadioError};
+use crate::radio::{Radio, ModulationFormat};
 mod radio;
+
+use std::{thread, time::Duration};
 
 macro_rules! input {
     {} => {{
@@ -20,9 +22,75 @@ macro_rules! input {
 }
 
 
-fn main() {
-    let mut radio = Radio::new().unwrap();
 
+fn run_async_coms() {
+    let mut radio = Radio::new().unwrap();
+    let mut radio2 = Radio::new().unwrap();
+
+    radio2.use_alt_address(0x35).expect("could not change address");
+    if !radio2.is_device_available() {
+        panic!("Radio 2 (0x35) was not found");
+    }
+
+    thread::spawn(move || {
+        loop {
+            radio.transmit("heartbeat".as_bytes()).expect("transmit error");
+
+            let packet = radio.get_packet().expect("Recieve Error");
+
+            match std::str::from_utf8(&packet) {
+                Ok(v) => {
+                    if v == "heartbeat" {
+                        break;
+                    }
+                },
+                Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+            };
+        }
+
+        radio.transmit("heartbeat".as_bytes()).expect("transmit error");
+
+        let mut i: i32 = 0;
+        loop {
+            let msg: String = "test msg ".to_string() +i.to_string().as_str();
+            radio.transmit(msg.as_bytes()).expect("transmit error");
+            i += 1
+        }
+    });
+
+    loop {
+        radio2.transmit("heartbeat".as_bytes()).expect("transmit error");
+
+        let packet = radio2.get_packet().expect("Recieve Error");
+
+        match std::str::from_utf8(&packet) {
+            Ok(v) => {
+                if v == "heartbeat" {
+                    break;
+                }
+            },
+            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+        };
+    }
+
+    loop {
+        let packet = radio2.get_packet().expect("Recieve Error");
+
+        let s = match std::str::from_utf8(&packet) {
+            Ok(v) => v,
+            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+        };
+
+        if s == "" {
+            continue;
+        }
+
+        println!("got packet \"{}\"", s);
+    }
+}
+
+fn run_cmd_int() {
+    let mut radio = Radio::new().unwrap();
     loop {
         match input!("w or r: ").as_str() {
             "w" => {
@@ -42,4 +110,30 @@ fn main() {
             _ => {},
         }
     }
+}
+
+fn tx(msg: &str, delay_ms: u64) {
+    let mut radio = Radio::new().unwrap();
+    loop {
+        radio.transmit(msg.as_bytes()).expect("tx failure");
+
+        thread::sleep(Duration::from_millis(delay_ms))
+    }
+}
+
+fn main() {
+    //run_async_coms();
+    //run_cmd_int();
+    tx("test beacon msg", 100);
+
+    let mut radio = Radio::new().unwrap();
+
+    //radio.set_frequency(101.1).expect("error");
+    //radio.set_power(101.1).expect("error");
+    //radio.set_deviation(101.1).expect("error");
+    //radio.set_symbol_rate(101.1).expect("error");
+    //radio.set_rx_filter(101.1).expect("error");
+    radio.set_modulation(ModulationFormat::GFSK4).expect("Error");
+
+
 }
