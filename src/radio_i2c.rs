a9867be0c3f502f7c2e5651bb6cd897debdea78d
Ethan Armstrong <w.ethan.armstrong@gmail.com>
2022-08-29 13:16:56 -0700

    added documentation for i2c functions

--- src/radio_i2c.rs
+++ src/radio_i2c.rs
@@ -30,7 +30,14 @@ pub enum RadioError {
     InvalidArgument,
 }
 
-
+/// Radio (I2C) is a driver for interfacing with STM32 based radios over I2C
+/// This library supports I2C communication on all linux based platforms
+/// 
+/// `Radio::new("I2C_PATH")` to get started
+/// 
+/// or
+/// 
+/// `Radio::new_rpi()` for raspberry pi os computers
 pub struct Radio {
     pub i2c: I2c,
     packet_wait_delay: u64,
@@ -39,6 +46,8 @@ pub struct Radio {
 }
 
 impl Radio {    
+    /// creates a new Radio object on the given i2c bus and checks
+    /// that the device detected is a Radio device
     pub fn new(i2c_path: &str) -> Result<Radio, RadioError> {
         let mut i2c = match I2c::new(i2c_path, ADDR) {
             Ok(n) => {n},
@@ -63,10 +72,13 @@ impl Radio {
         Ok(Radio { i2c, packet_wait_delay: 10, write_wait_delay: 10})
     }
 
+    /// creates a new Radio object on the default rpi i2c bus and
+    /// checks that the device detected is a Radio device
     pub fn new_rpi() -> Result<Radio, RadioError> {
         return Radio::new("/dev/i2c-1")
     }
 
+    /// switches to the pre-programmed backup address for the Radio
     pub fn use_alt_address(&mut self) -> Result<(), RadioError> {
         self.i2c = match I2c::new("/dev/i2c-1", BACKUP_ADDR) {
             Ok(n) => n,
@@ -78,14 +90,32 @@ impl Radio {
         Ok(())
     }
 
+    /// sets the delay between requesting a packet from the Radio
+    /// and expecting the packet to come through
+    /// 
+    /// this gives the Radio time to gather the packet before i2c times out
+    /// on non clock-stetching devices
+    /// 
+    /// ## Default
+    /// 10 ms
     pub fn set_packet_gather_delay(&mut self, delay: u64) {
         self.packet_wait_delay = delay;
     }
 
+    /// sets the delay between requesting to write a packet to the radio
+    /// and sending the packet to the radido
+    /// 
+    /// this gives the Radio time to ready itself for recieve before i2c times out
+    /// on non clock-stetching devices
+    /// ## Default
+    /// 10 ms
     pub fn set_write_wait_delay(&mut self, delay: u64) {
         self.write_wait_delay = delay;
     }
 
+    /// queries the port to check if the radio is available
+    /// 
+    /// useful for in-constructor checks
     fn check_for_device(i2c: &mut I2c) -> bool {
         let mut buf: [u8; IDENT_MSG.len()] = [0u8; IDENT_MSG.len()];
         match i2c.read(&mut buf) {
@@ -98,6 +128,10 @@ impl Radio {
         return buf == IDENT_MSG.as_bytes();
     }
 
+    /// queries the radio and checks if it is available
+    /// 
+    /// ## Returns
+    /// wheather or not a device was found
     pub fn is_device_available(&mut self) -> bool{
         return Radio::check_for_device(&mut self.i2c);
     }
@@ -107,6 +141,7 @@ impl Radio {
 
 // transmit / recieve
 impl Radio {
+    /// transmits the given message
     pub fn transmit(&mut self, msg: &[u8]) -> Result<(), RadioError> {
 
         if msg.len() > u8::MAX as usize {
@@ -137,6 +172,12 @@ impl Radio {
         Ok(())
     }
 
+    /// gets the most recent packet stored on the Radio
+    /// 
+    /// ## Returns
+    /// Vec/<u8/> with the byte data of the packet
+    /// 
+    /// empty Vec/<u8/> if no available packet was found
     pub fn get_packet(&mut self) -> Result<Vec<u8>, RadioError> {
         // send read command
         let msg: [u8; 5] = [0x02, 0x00, 0x00, 0x00, 0x00];
@@ -182,6 +223,7 @@ impl Radio {
         Ok(out)
     }
 
+    /// sends a reset command to reset the onboard Radio chip
     pub fn radio_reset(&mut self) -> Result<(), RadioError> {
         match self.i2c.write(&[9,0,0,0,0]) {
             Ok(_) => {},
@@ -192,6 +234,7 @@ impl Radio {
         Ok(())
     }
 
+    /// sends a reset command to perform a soft reset on the entire board
     pub fn soft_reset(&mut self) -> Result<(), RadioError> {
         match self.i2c.write(&[10,0,0,0,0]) {
             Ok(_) => {},
@@ -202,6 +245,7 @@ impl Radio {
         Ok(())
     }
 
+    /// performs a hard reset using the boards reset pin
     pub fn reset(&mut self) -> Result<(), RadioError> {
         println!("placeholder function, no action occured");
         Ok(())
@@ -211,6 +255,7 @@ impl Radio {
 
 // change radio settings
 impl Radio {
+    /// helper function to write the byte data of a f32 over i2c
     fn set_float_val(&mut self, cmd: u8, val: f32) -> Result<(), RadioError> {
         let bytes = val.to_ne_bytes();
         let mut buf: [u8; 5] = [cmd, 0x00, 0x00, 0x00, 0x00];
@@ -229,26 +274,42 @@ impl Radio {
         return Ok(());
     }
 
+    /// attempts to set the frequency of the radio
+    /// 
+    /// there is a chance the radio rejects the value if it is invalid
     pub fn set_frequency(&mut self, frequency: f32) -> Result<(), RadioError> {
         return self.set_float_val(0x03, frequency);
     }
 
+    /// attempts to set the tx gain of the radio
+    /// 
+    /// there is a chance the radio rejects the value if it is invalid
     pub fn set_power(&mut self, power: f32) -> Result<(), RadioError> {
         return self.set_float_val(0x04, power);
     }
 
+    /// attempts to set the FSK bandwith of the radio
+    /// 
+    /// there is a chance the radio rejects the value if it is invalid
     pub fn set_deviation(&mut self, deviation: f32) -> Result<(), RadioError> {
         return self.set_float_val(0x05, deviation);
     }
 
+    /// attempts to set the symbol rate of the radio
+    /// 
+    /// there is a chance the radio rejects the value if it is invalid
     pub fn set_symbol_rate(&mut self, symbol_rate: f32) -> Result<(), RadioError> {
         return self.set_float_val(0x06, symbol_rate);
     }
 
+    /// attempts to set the recieve filter of the radio
+    /// 
+    /// there is a chance the radio rejects the value if it is invalid
     pub fn set_rx_filter(&mut self, rx_filter: f32) -> Result<(), RadioError> {
         return self.set_float_val(0x07, rx_filter);
     }
 
+    /// sets the modulation mode of the radio
     pub fn set_modulation(&mut self, mode: ModulationFormat) -> Result<(), RadioError> {
         let buf: [u8; 5] = [0x08, mode as u8, 0x00, 0x00, 0x00];
 
