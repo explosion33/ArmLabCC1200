5d68ea34379778a5d8517aa980942f497b6641e9
Ethan Armstrong <w.ethan.armstrong@gmail.com>
2022-08-29 13:04:15 -0700

    Added documentation for all functions and structs removed unrwap and except statements in favor of returning Err(RaidioError)

--- src/radio_serial.rs
+++ src/radio_serial.rs
@@ -27,6 +27,12 @@ pub enum RadioError {
     PortDetectError,
 }
 
+/// Radio (Serial) is a driver for interfacing with STM32 based radios over serial
+/// This library supports serial communication over all platforms
+/// 
+/// `Radio::new("PORT_NAME")` to get started
+/// 
+/// `get_open_ports` or `get_radio_ports` to see options for `PORT_NAME`
 pub struct Radio {
     port: Box<dyn SerialPort>,
     port_path: String,
@@ -34,6 +40,11 @@ pub struct Radio {
 
 // init
 impl Radio {
+    /// creates a new Radio object on the given port
+    /// 
+    /// runs existence checks, and attempts to synchronize radio
+    /// 
+    /// see `Radio::new_bare` for a constructor without overhead
     pub fn new(path: &str) -> Result<Radio, RadioError> {
         let mut port = match serialport::new(path, 115200)
             .timeout(Duration::from_millis(100))
@@ -42,10 +53,16 @@ impl Radio {
                 Err(_) => {return Err(RadioError::PortOpenError)}
         };
 
-        port.write_data_terminal_ready(true).unwrap();
+        match port.write_data_terminal_ready(true) {
+            Ok(_) => {},
+            Err(_) => {return Err(RadioError::WriteError);},
+        };
 
 
-        let i = Radio::sync_serial(&mut port, 6).expect("msg");
+        let i = match Radio::sync_serial(&mut port, 6) {
+            Ok(n) => n,
+            Err(n) => {return Err(n);},
+        };
 
         println!("synced radio, after {} bytes | device was {} steps ahead", i, 6-i);
 
@@ -53,12 +70,22 @@ impl Radio {
             return Err(RadioError::DevciceDetectError);
         }
 
-        let i = Radio::sync_serial(&mut port, 6).expect("msg");
+        let i = match Radio::sync_serial(&mut port, 6) {
+            Ok(n) => n,
+            Err(n) => {return Err(n);},
+        };
+        
         println!("synced radio, after {} bytes | device was {} steps ahead", i, 6-i);
 
         Ok(Radio {port, port_path: path.to_string()})
     }
 
+    /// creates a new Radio object on the given port
+    /// without checking for existence or attempting to synchronize
+    /// just simply opens the port and returns an object
+    /// 
+    /// not recommended for the general use case
+    /// see `Radio::new_bare` for implemented existence and synchronization
     pub fn new_bare(path: &str) -> Result<Radio, RadioError> {
         let mut port = match serialport::new(path, 115200)
             .timeout(Duration::from_millis(100))
@@ -76,10 +103,18 @@ impl Radio {
         Ok(Radio {port, port_path: path.to_string()})
     }
 
+
+    /// queries the radio and checks if it is available
+    /// 
+    /// ## Returns
+    /// wheather or not a device was found
     pub fn is_device_available(&mut self) -> bool {
         return Radio::check_for_device(&mut self.port);
     }
 
+    /// queries the port to check if the radio is available
+    /// 
+    /// useful for in-constructor checks
     fn check_for_device(port: &mut Box<dyn SerialPort>) -> bool {
         let cmd: [u8; 6] = [0,0,0,0,0, '\n' as u8];
         match port.write_all(&cmd) {
@@ -108,6 +143,13 @@ impl Radio {
         return true;
     }
 
+    /// ensures the command queue is in sync with the given port
+    /// 
+    /// sometimes on intialization Windows will send 1-4 extra bytes causing
+    /// the command queue to be out of sync
+    /// 
+    /// this sends a byte at a time, then checks for output, until it reaches timeout
+    /// or it detects output
     fn sync_serial(port: &mut Box<dyn SerialPort>, timeout_iter: usize) -> Result<usize, RadioError> {
         let mut i: usize = 0;
 
@@ -131,6 +173,10 @@ impl Radio {
 
     }
 
+    /// ensures the Radio command queue is in sync
+    /// 
+    /// ## Returns
+    /// number of bytes sent in order to sync Radio
     pub fn sync(&mut self, timeout_iter: usize) -> Result<usize, RadioError>  {
         return Radio::sync_serial(&mut self.port, timeout_iter);
     }
@@ -139,6 +185,9 @@ impl Radio {
 
 // transmit / recieve
 impl Radio {
+    /// helper function to write bytes to Serial
+    /// 
+    /// ensures the bytes were all flushed 
     fn write_bytes(&mut self, data: &[u8]) -> Result<(), RadioError> {
         // create temp vector to hold both message and newline
         let mut middle: Vec<u8> = vec![0; 0];
@@ -161,6 +210,7 @@ impl Radio {
         }
     }
 
+    /// transmits the given message
     pub fn transmit(&mut self, msg: &[u8]) -> Result<(), RadioError> {
         if msg.len() > u8::MAX as usize {
             return Err(RadioError::InvalidArgument);
@@ -195,6 +245,12 @@ impl Radio {
         Ok(())
     }
 
+    /// gets the most recent packet stored on the Radio
+    /// 
+    /// ## Returns
+    /// Vec/<u8/> with the byte data of the packet
+    /// 
+    /// empty Vec/<u8/> if no available packet was found
     pub fn get_packet(&mut self) -> Result<Vec<u8>, RadioError> {
         let cmd: [u8; 5] = [0x2, 0x0, 0x0, 0x0, 0x0];
         match self.write_bytes(&cmd) {
@@ -228,21 +284,20 @@ impl Radio {
         Ok(out)
     }
 
+    /// sends a reset command to reset the onboard Radio chip
     pub fn radio_reset(&mut self) -> Result<(), RadioError> {
         return self.write_bytes(&[9,0,0,0,0]);
     }
 
+    /// sends a reset command to perform a soft reset on the entire board
     pub fn soft_reset(&mut self) -> Result<(), RadioError> {
         return self.write_bytes(&[10,0,0,0,0]);
     }
 
-    pub fn reset(&mut self) -> Result<(), RadioError> {
-        println!("placeholder function, no action occured");
-        Ok(())
-    }
 }
 
 impl Radio {
+    /// helper function to write the byte data of a f32 to the serial port
     fn set_float_val(&mut self, cmd: u8, val: f32) -> Result<(), RadioError> {
         let bytes = val.to_ne_bytes();
         let mut buf: [u8; 5] = [cmd, 0x00, 0x00, 0x00, 0x00];
@@ -261,26 +316,42 @@ impl Radio {
         return Ok(());
     }
 
+    /// attempts to set the frequency of the radio
+    /// 
+    /// there is a chance the radio rejects the value if it is invalid
     pub fn set_frequency(&mut self, frequency: f32) -> Result<(), RadioError> {
         return self.set_float_val(0x03, frequency);
     }
 
+    /// attempts to set the tx gain of the radio
+    /// 
+    /// there is a chance the radio rejects the value if it is invalid
     pub fn set_power(&mut self, power: f32) -> Result<(), RadioError> {
         return self.set_float_val(0x04, power);
     }
 
+    /// attempts to set the FSK bandwith of the radio
+    /// 
+    /// there is a chance the radio rejects the value if it is invalid
     pub fn set_deviation(&mut self, deviation: f32) -> Result<(), RadioError> {
         return self.set_float_val(0x05, deviation);
     }
 
+    /// attempts to set the symbol rate of the radio
+    /// 
+    /// there is a chance the radio rejects the value if it is invalid
     pub fn set_symbol_rate(&mut self, symbol_rate: f32) -> Result<(), RadioError> {
         return self.set_float_val(0x06, symbol_rate);
     }
 
+    /// attempts to set the recieve filter of the radio
+    /// 
+    /// there is a chance the radio rejects the value if it is invalid
     pub fn set_rx_filter(&mut self, rx_filter: f32) -> Result<(), RadioError> {
         return self.set_float_val(0x07, rx_filter);
     }
 
+    /// sets the modulation mode of the radio
     pub fn set_modulation(&mut self, mode: ModulationFormat) -> Result<(), RadioError> {
         let buf: [u8; 5] = [0x08, mode as u8, 0x00, 0x00, 0x00];
 
@@ -295,6 +366,7 @@ impl Radio {
     }
 }
 
+/// gets a Vector containing the names of all connected serial ports on the system
 pub fn get_open_ports() -> Result<Vec<String>, RadioError> {
     let res = match available_ports() {
         Ok(n) => n,
@@ -310,6 +382,7 @@ pub fn get_open_ports() -> Result<Vec<String>, RadioError> {
     return Ok(out);
 }
 
+/// gets a Vector containing the names of all connected Radio objects
 pub fn get_radio_ports() -> Result<Vec<String>, RadioError> {
     let mut out: Vec<String> = vec![];
     let res = match available_ports() {
